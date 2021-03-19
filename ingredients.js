bc870f7b6c09ebdee7c310d37576a87d2b614c07
SanguNikhil <79975024+SanguNikhil@users.noreply.github.com>
2021-03-19 11:19:11 +0530

    Add files via upload

---
+++ ingredients.js
@@ -0,0 +1,16 @@
+// ½ cups white sugar
+// eggs
+// tablespoons cocoa
+// tablespoons red food coloring
+// teaspoon salt
+// teaspoon vanilla extract
+// cup buttermilk
+// ½ cups sifted all-purpose flour
+// ½ teaspoons baking soda
+// tablespoon distilled white vinegar
+// Icing:
+// tablespoons all-purpose flour
+// cup milk
+// cup white sugar
+// cup butter, room temperature
+// teaspoon vanilla extract
